7e3cb38b1519266850c9981b647edfdc914d2242
rushomrong <rushomarak@gmail.com>
2024-09-02 01:26:53 +0600

    finding odd numbers from 61 to 100

---
+++ practice task/findoddnum.js
@@ -0,0 +1,11 @@
+/***
+
+Subtask-1:
+
+Find all the odd numbers from 61 to 100.
+
+ */
+
+for (let oddNumber = 61; oddNumber <= 100; oddNumber = oddNumber + 2) {
+  console.log("Your odd numbers are:", oddNumber);
+}
