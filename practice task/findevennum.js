ed364f87fcb4b67ba2b95c6e315e62fecf7628f6
rushomrong <rushomarak@gmail.com>
2024-09-02 01:43:58 +0600

    find even number

---
+++ practice task/findevennum.js
@@ -0,0 +1,11 @@
+/*
+Subtask-2:
+
+Find all the even numbers from 78 to 98.
+
+
+/*programming hero*/
+
+for (let evenNum = 78; evenNum <= 98; evenNum = evenNum + 2) {
+  console.log("Your even numbers are :", evenNum);
+}
