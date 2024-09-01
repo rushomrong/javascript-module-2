27834a046912c4cf39164265c6f202382ff97067
rushomrong <rushomarak@gmail.com>
2024-09-02 01:02:55 +0600

    practice task 60 times displaying loop added

---
+++ practice task/60times.js
@@ -0,0 +1,15 @@
+/***
+ * Task - 1
+
+"I will invest at least 6 hrs every single day for next 60 days!" this message 60 times. So display this.
+
+ */
+
+/*programming hero*/
+
+for (let times = 1; times <= 60; times++) {
+  console.log(
+    times,
+    "I will invest at least 6 hrs every single day for next 60 days!"
+  );
+}
