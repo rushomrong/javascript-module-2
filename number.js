5e81ef22cbe4ec83a6891ac9c08f1f8f18f521c3
rushomrong <rushomarak@gmail.com>
2024-09-01 21:18:18 +0600

    Added sum condition ++

---
+++ number.js
@@ -0,0 +1,10 @@
+//this condition will will loop ++ number but sum with the number value what the value is getting the after counting the number until the condition is true
+let num = 1;
+let sum = 0;
+
+while (num <= 10) {
+  console.log(num);
+  sum = sum + num;
+  console.log("sum:", sum);
+  num++;
+}
