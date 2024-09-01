8dd5dce94e636b519ff8202d4fce8993522ad57e
rushomrong <rushomarak@gmail.com>
2024-09-01 22:21:46 +0600

    forloop condition added

---
+++ forloop.js
@@ -0,0 +1,17 @@
+//for loop
+// 1st part : loop variable declaration
+// 2nd part  : loop condition
+// 3rd part : loop variable change
+for (let num = 0; num < 5; num++) {
+  console.log(num);
+}
+
+//even numbers
+for (let i = 0; i <= 20; i = i + 2) {
+  console.log("even number is:", i);
+}
+
+// Odd numbers
+for (let i = 1; i <= 20; i = i + 2) {
+  console.log("Your odd number is:", i);
+}
