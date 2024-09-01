ff18419743a018b48f61d26ae7c315ec3209ae25
rushomrong <rushomarak@gmail.com>
2024-09-01 21:28:06 +0600

    even number checking using while loop

---
+++ evennumbers.js
@@ -0,0 +1,13 @@
+//displaying even numbers
+
+let num = 1;
+while (num <= 10) {
+  console.log(num);
+
+  //checking the number is even
+  if (num % 2 === 0) {
+    console.log("even number is:", num);
+  }
+
+  num++;
+}
